51f7bea7f1f6f45f4542f3bbd3ec255bda9ad80e
Subhadeep <abhisubhadeep@gmail.com>
2022-03-27 22:26:50 +0530

    All done

---
+++ home-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>home page</title>
+<body>
+    <div class="container-fluid">
+        <div class="col-sm-4">
+    <form action="land-2.php" method="post">
+        <br>
+        <label for="age" class="form-label">Enter User Age :</label><br>
+        <input type="number" id="age" name="age" class="form-control" value=""><br>
+        <input type="submit" class="btn btn-danger" value="Submit">
+    </form>
+</body>
+
+</html>
